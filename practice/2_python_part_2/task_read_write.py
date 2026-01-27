ee6770832d1a8db7e7485e09f8ccf372a4c2c5fc
eaghalovyan <eaghalovyan@griddynamics.com>
2026-01-27 16:44:23 +0400

    Read-Write task done

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -13,3 +13,22 @@ Example:
 
     result.txt(content: "23, 78, 3")
 """
+
+import os
+from natsort import natsorted
+folder = "./files"
+
+def combine_file_values(input_folder, result_file = "result.txt"):
+    values = []
+    for filename in natsorted(os.listdir(input_folder)):
+        file_path = os.path.join(input_folder, filename)
+        if os.path.isfile(file_path):
+            with open(file_path, "r") as f:
+                content = f.read().strip()
+                if content:
+                    values.append(content)
+        
+    with open(result_file, "w") as f:
+            f.write(", ".join(values))
+
+combine_file_values(folder)
