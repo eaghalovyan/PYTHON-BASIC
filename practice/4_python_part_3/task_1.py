6ffc86a9894d85a96aa35acbbf1ae4e9649f335f
eaghalovyan <eaghalovyan@griddynamics.com>
2026-02-09 13:38:11 +0400

    Task 1 and its tests are written

--- practice/4_python_part_3/task_1.py
+++ practice/4_python_part_3/task_1.py
@@ -10,15 +10,39 @@ If entered date is from future, return negative value for number of days
     >>> calculate_days('10-07-2021')
     WrongFormatException
 """
-from datetime import datetime
+from datetime import datetime, date
 
+class WrongFormatException(Exception):
+    pass
 
 def calculate_days(from_date: str) -> int:
-    ...
-
+    today = date.today()
+    try:
+        given_date = datetime.strptime(from_date, "%Y-%m-%d").date()
+        return (today - given_date).days
+    except ValueError:
+        raise WrongFormatException("Wrong date format")
+       
+# print(calculate_days("2026-12-05"))
 
 """
 Write tests for calculate_days function
 Note that all tests should pass regardless of the day test was run
 Tip: for mocking datetime.now() use https://pypi.org/project/pytest-freezegun/
 """
+
+import pytest
+from freezegun import freeze_time
+
+@freeze_time("2026-02-09")
+def test_calculate_days():
+    assert calculate_days("2026-02-05") == 4
+    assert calculate_days("2026-02-10") == -1
+    assert calculate_days("2026-02-09") == 0
+
+@freeze_time("2026-02-09")
+def test_wrong_format():
+    with pytest.raises(WrongFormatException):
+        calculate_days("2026/02/09")
+    with pytest.raises(WrongFormatException):
+        calculate_days("09-02-2026")
