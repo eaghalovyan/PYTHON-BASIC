564dd363ad49991811b8072d63e3c101e2e7974a
eaghalovyan <eaghalovyan@griddynamics.com>
2026-02-09 15:29:33 +0400

    Task 2 and its tests are implemented

--- practice/4_python_part_3/task_2.py
+++ practice/4_python_part_3/task_2.py
@@ -11,11 +11,44 @@ Examples:
 """
 import math
 
+class OperationNotFoundException(Exception):
+    pass
 
 def math_calculate(function: str, *args):
-    ...
+        try:
+            func = getattr(math, function)
+        except AttributeError:
+            raise OperationNotFoundException
+        
+        if len(args) > 2 or len(args) == 0:
+             raise ValueError("Function must take 1 or 2 arguments")
+        return func(*args)  
+        
+    
 
 
 """
 Write tests for math_calculate function
 """
+import pytest
+@pytest.mark.parametrize(
+    "function, args, expected",
+    [
+        ("log", (1024, 2), 10.0),   # 2 args
+        ("ceil", (10.7,), 11),       # 1 arg
+        ("sqrt", (16,), 4.0)         # 1 arg    
+    ])
+def test_math_calculate(function, args, expected):
+     assert math_calculate(function, *args) == expected
+     
+@pytest.mark.parametrize(
+    "function, args, expected",
+     [
+        ("hi", (16,), OperationNotFoundException),
+        ("log", (), ValueError),
+        ("log", (1,2,3), ValueError)
+     ]
+)
+def test_math_calculate_exceptions(function, args, expected):
+     with pytest.raises(expected):
+          math_calculate(function, *args)
